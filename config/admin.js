b03abf43ee93d0b2e8aaf65df8511b61da44608d
Mrityunjay321 <mrutunjay@algoengines.com>
2023-01-25 19:43:23 +0530

    strapi be change"

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "J2fiTWa/L8NnI/fonXWNjA=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "usJ915pIHGlmArmwJifSpA=="),
   },
 });
